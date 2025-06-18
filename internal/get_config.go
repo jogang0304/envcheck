1dee7f1e1f3550a90155d75f771f68fb10beb70b
jogang <jogang0304@gmail.com>
2025-06-18 15:43:33 +0300

    Wrote reading config

--- internal/get_config.go
+++ internal/get_config.go
@@ -1,5 +1,12 @@
 package internal
 
+import (
+	"errors"
+	"os"
+
+	"gopkg.in/yaml.v3"
+)
+
 type SupportedVarType string
 
 const (
@@ -11,21 +18,31 @@ const (
 )
 
 type VarEntry struct {
-	Name         string
-	Required     bool
-	Type         SupportedVarType
-	DefaultValue any
-	Pattern      string
+	Name         string           `yaml:"name"`
+	Required     bool             `yaml:"required"`
+	Type         SupportedVarType `yaml:"type"`
+	DefaultValue any              `yaml:"default_value"`
+	Pattern      string           `yaml:"pattern"`
 }
 
 type Config struct {
-	Vars []VarEntry
+	Vars []VarEntry `yaml:"vars"`
 }
 
 /*
 This function reads .env.yaml file and returns config.
 */
 func GetConfig() (Config, error) {
-	// TODO
-	return Config{}, nil
+	data, err := os.ReadFile(".env.yaml")
+	if err != nil {
+		return Config{}, errors.Join(errors.New("Failed to read .env.yaml"), err)
+	}
+
+	var config Config
+	err = yaml.Unmarshal(data, &config)
+	if err != nil {
+		return Config{}, errors.Join(errors.New("Failed to unmarshal .env.config. Probably incorrect yaml structure"), err)
+	}
+
+	return config, nil
 }
---
+++ internal/get_config_test.go
@@ -0,0 +1,113 @@
+package internal_test
+
+import (
+	"os"
+	"strings"
+	"testing"
+
+	"reflect"
+
+	"github.com/jogang0304/envcheck/internal"
+)
+
+func TestGetConfig(t *testing.T) {
+	t.Run("Valid config file", func(t *testing.T) {
+		const configFileContent = `
+vars:
+  - name: firstVar
+    required: false
+    type: int
+    default_value: 0
+  - name: secondVar
+    required: true
+    type: string
+    pattern: .*secret.*
+`
+		var expectedConfig internal.Config = internal.Config{
+			Vars: []internal.VarEntry{{
+				Name:         "firstVar",
+				Required:     false,
+				Type:         internal.IntType,
+				DefaultValue: 0,
+				Pattern:      "",
+			}, {
+				Name:         "secondVar",
+				Required:     true,
+				Type:         internal.StringType,
+				Pattern:      ".*secret.*",
+				DefaultValue: nil,
+			}},
+		}
+
+		tempdir := t.TempDir()
+
+		envFilePath := tempdir + "/.env.yaml"
+		err := os.WriteFile(envFilePath, []byte(configFileContent), 0644)
+		if err != nil {
+			t.Fatalf("failed to create .env.config file: %v", err)
+		}
+
+		os.Chdir(tempdir)
+
+		c, err := internal.GetConfig()
+		if err != nil {
+			t.Fatalf("failed to read env config: %v", err)
+		}
+
+		configIsCorrect := reflect.DeepEqual(c, expectedConfig)
+
+		if !configIsCorrect {
+			t.Errorf("Expected config: %v\n Actual config: %v", expectedConfig, c)
+		}
+	})
+
+	t.Run("Invalid yaml file", func(t *testing.T) {
+		const configFileContent = `
+vars:
+  -required: false
+    type: int
+    default_value: 0
+  - name: secondVar
+required: true
+`
+		const expectedErrorText = "Failed to unmarshal .env.config. Probably incorrect yaml structure"
+
+		tempdir := t.TempDir()
+
+		envFilePath := tempdir + "/.env.yaml"
+		err := os.WriteFile(envFilePath, []byte(configFileContent), 0644)
+		if err != nil {
+			t.Fatalf("failed to create .env.config file: %v", err)
+		}
+
+		os.Chdir(tempdir)
+
+		_, err = internal.GetConfig()
+		if err == nil {
+			t.Fatal("expected to get an error")
+		}
+
+		hasExpectedError := strings.Contains(err.Error(), expectedErrorText)
+		if !hasExpectedError {
+			t.Fatalf("expected error \"%v\", got \"%v\"", expectedErrorText, err)
+		}
+	})
+
+	t.Run("No .env.yaml file", func(t *testing.T) {
+		const expectedErrorText = "Failed to read .env.yaml"
+
+		tempdir := t.TempDir()
+
+		os.Chdir(tempdir)
+
+		_, err := internal.GetConfig()
+		if err == nil {
+			t.Fatal("expected to get an error")
+		}
+
+		hasExpectedError := strings.Contains(err.Error(), expectedErrorText)
+		if !hasExpectedError {
+			t.Fatalf("expected error \"%v\", got \"%v\"", expectedErrorText, err)
+		}
+	})
+}
